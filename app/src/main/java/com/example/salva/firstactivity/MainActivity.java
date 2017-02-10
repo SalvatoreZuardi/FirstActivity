10c44dc24352e6770eeee89119b7d78543d207c2
Salvatore Zuardi <Salvatore Zuardi>
2017-02-10 10:41:59 +0100

    ActivityMain Register Login

---
+++ app/src/main/java/com/example/salva/firstactivity/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.salva.firstactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by salva on 08/02/2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
