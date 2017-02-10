bf168f56b2f6f2ce385924596c14222703deae6c
Salvatore Zuardi <Salvatore Zuardi>
2017-02-10 13:23:38 +0100

    connecting beetween activity

---
+++ app/src/main/java/com/example/salva/firstactivity/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.example.salva.firstactivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by salva on 10/02/2017.
+ */
+
+public class LoginActivity extends Activity{
+    EditText username_ET;
+    EditText password_ET;
+
+    Button login_btn;
+
+    View.OnClickListener loginClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String username = username_ET.getText().toString();
+            String password = password_ET.getText().toString();
+            if (doLogin(username, password)) {
+                Toast.makeText(LoginActivity.this,R.string.toast, Toast.LENGTH_LONG).show();
+            }
+            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        username_ET=(EditText)findViewById(R.id.login_username_et);
+        password_ET=(EditText)findViewById(R.id.login_password_et);
+        login_btn=(Button)findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(loginClickListener);
+    }
+
+    private boolean doLogin(String username, String password) {
+        return true;
+    }
+
+}
--- app/src/main/java/com/example/salva/firstactivity/MainActivity.java
+++ app/src/main/java/com/example/salva/firstactivity/MainActivity.java
@@ -2,19 +2,21 @@ package com.example.salva.firstactivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by salva on 08/02/2017.
  */
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
-
-
 }
